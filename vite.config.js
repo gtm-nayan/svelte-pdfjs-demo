de2aed1b2d674a4a63789eaf79057f5a63b4992b
Nayan Gautam <gtmnayan@gmail.com>
2024-05-12 19:12:02 +0545

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,23 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		{
+			name: 'svelte-pdfjs',
+			config(c) {
+				((c.optimizeDeps ?? {}).exclude ??= []).push('pdfjs-dist');
+				(c.build ??= {}).target = 'esnext';
+			},
+			resolveDynamicImport(specifier, importer) {
+				if (
+					typeof specifier === 'string' &&
+					importer.includes('node_modules/pdfjs-dist')
+				)
+					return false;
+			},
+			transform(code, id, options) {
+				if (id.includes('node_modules/pdfjs-dist') && !options?.ssr)
+					return code.replaceAll(/\w+\.isNodeJS/g, 'false');
+			},
+		},
 	],
 });
