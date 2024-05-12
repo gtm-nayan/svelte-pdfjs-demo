33d3fb0fbae770d085807d6e83eb03c37e277b2b
Nayan Gautam <gtmnayan@gmail.com>
2024-05-12 19:11:32 +0545

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Document, Page } from 'svelte-pdfjs';
+	import { set_pdfjs_context } from 'svelte-pdfjs/vite';
+
+	set_pdfjs_context();
+</script>
+
+<Document file="/hi.pdf">
+	<Page num={1} />
+</Document>
